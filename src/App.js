67520f004911efc1eea96056a6b2070139b1f530
TekuEatsHomework <tselmuun0816@gmail.com>
2022-11-05 11:21:55 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (  
+    <div className="App2">
+      <div className='leftside2'>
+        <img className='child' src='https://s3-alpha-sig.figma.com/img/967e/b14e/6fc29dfe6ee46cdd918b8c8b106d5e66?Expires=1668384000&Signature=Moi3Nl5aFZibz1BCdDNVXNJCsUqFI2BW2C8heA5SJrdLlv19b3NG37cnaGouGtoGtxDmV-~MZvgxg0UEwNlUwCBDlwCxle0LhYZFsE4E40b5OjmTWyk~IJymXlYt37q6-n-HpeTz33uC2zfA9Zw44VSKCMkfCyhQVCWO7F8orUxE-epZDh5cj8cHhMXOr5a63bos2MPOQlYsUIY~pxfDhffONvourJ7WBsymdU028wk0iJeGAYqPTXm6aCiMa9vBaSkUNVXzXSFPQxXirGFYExP~jQIhiStY4n0m1xsGobxJDs~AwGzrVwUSLHky0S8sWu2tmni-cChdrjDr5iyOMg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'></img>
+      </div>
+      </div>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
+<link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@1,1000display=swap" rel="stylesheet"></link>
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
